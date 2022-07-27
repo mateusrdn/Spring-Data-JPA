59e42a24c903a534f61d82dfb38f3b89a2e8b562
Mateus Rodrigues do Nascimento <90523108+mateusrdn@users.noreply.github.com>
2022-07-27 16:43:04 -0300

    Tabela cargos

---
+++ src/main/java/com/spring/data/jpa/curso/orm/Cargo.java
@@ -0,0 +1,30 @@
+package com.spring.data.jpa.curso.orm;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cargos")
+public class Cargo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String descricao;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
